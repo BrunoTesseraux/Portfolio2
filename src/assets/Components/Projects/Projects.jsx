790831e88d10027bce70bd3182497e103099f050
Bruno <bruno.tesseraux@gmx.de>
2024-05-10 16:24:25 +0200

    links eingebaut

--- src/assets/Components/Projects/Projects.jsx
+++ src/assets/Components/Projects/Projects.jsx
@@ -7,22 +7,26 @@ const Projects = () => {
         {
             title: "Erstes Portfolio",
             screenshot: "./portfolio1.png",
-            logo: "./projekt1.png"
+            logo: "./projekt1.png",
+            link: "https://portfolio-tawny-chi-23.vercel.app/"
         },
         {
             title: "Poke API",
             screenshot: "./pokeAPI.png",
-            logo: "projekt2.png"
+            logo: "projekt2.png",
+            link: "https://poke-api-plum.vercel.app/"
         },   
         {
             title: "Movie DB",
             screenshot: "./movieDB.png",
-            logo: "projekt3.png"
+            logo: "projekt3.png",
+            link: "https://movie-db-blush-seven.vercel.app/"
         },
         {
             title: "GrosShop",
             screenshot: "url-zum-screenshot-2",
-            logo: "projekt4.png"
+            logo: "projekt4.png",
+            link: ""
         },
     ];
 
--- src/assets/Components/SingleProject/SingleProject.jsx
+++ src/assets/Components/SingleProject/SingleProject.jsx
@@ -2,11 +2,11 @@ import "./SingleProject.scss";
 
 const SingleProject = ({ project }) => { // Nehmen Sie project als Prop an
     return ( 
-        <div className="single-project">
+        <a href={project.link} target="_blank" className="single-project">
             <img src={project.screenshot} className="screenshot" alt="Screenshot" />
             <img src={project.logo} className="logo" alt="Logo" />
             <p>{project.title}</p>
-        </div>
+        </a>
      );
 }
  
