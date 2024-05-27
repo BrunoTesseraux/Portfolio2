0777a778d72e3a2c8c48c868cc4f5d7641be88e8
Bruno <bruno.tesseraux@gmx.de>
2024-05-27 20:35:24 +0200

    styling

--- src/assets/Components/SingleProject/SingleProject.jsx
+++ src/assets/Components/SingleProject/SingleProject.jsx
@@ -5,7 +5,8 @@ const SingleProject = ({ project }) => { // Nehmen Sie project als Prop an
         <a href={project.link} target="_blank" className="single-project">
             <img src={project.screenshot} className="screenshot" alt="Screenshot" />
             <img src={project.logo} className="logo" alt="Logo" />
-            <p>{project.title}</p>
+            <p className="project-title">{project.title}</p>
+            <p className="beschreibung">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quidem recusandae nulla beatae iste illum, quod voluptatum officiis rem amet!</p>
         </a>
      );
 }
